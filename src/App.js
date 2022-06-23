f6367b70ae513e79afe5cd22edfbcf7c440c9161
Risto Koverola <risto.koverola@eficode.com>
2022-06-23 13:17:41 +0300

    Add login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +13,26 @@ const App = () => {
     )  
   }, [])
 
+  const handleLoginSubmit = (event) => {
+    event.preventDefault()
+    console.log('Got login', username, password)
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Log in to the application</h2>
+      <form onSubmit={handleLoginSubmit} >
+        <div>
+          Username <input type={'text'} onChange={({ target }) => { setUsername(target.value) }} />
+        </div>
+        <div>
+          Password <input type={'password'} onChange={({ target }) => { setPassword(target.value) }} />
+        </div>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+      <h2>Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
