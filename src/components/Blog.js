fc862ed6ed0fd90b5a305f1d5bb87b570b3ccc9d
Risto Koverola <risto.koverola@eficode.com>
2022-06-27 13:39:28 +0300

    Solve 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  
+  const [minimized, setMinimized] = useState(true)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const additionalInfoStyle = minimized
+    ? { display: 'none' }
+    : { display: '' }
+  
+  const buttonText = minimized
+    ? 'View'
+    : 'Hide'
+
+  const toggleMinimized = () => { setMinimized(!minimized) }
+
+  return (
+  <div style={blogStyle} >
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={toggleMinimized} >{buttonText}</button>
+    </div>
+    <div style={additionalInfoStyle} >
+      <div>{blog.url}</div>
+      <div>
+        Likes: {blog.likes}
+        <button>Like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>
   </div>  
-)
+)}
 
 export default Blog
